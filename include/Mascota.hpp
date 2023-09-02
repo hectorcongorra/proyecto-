0db5c3db1b4a6ac97f6a0fbbd696b45cc51fb98b
hectorcongorra <143230170+hectorcongorra@users.noreply.github.com>
2023-09-02 01:05:12 +0000

    clase viernes

---
+++ include/Mascota.hpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+class Mascota
+{
+private:
+    int vida;
+    std::string nombre;
+public:
+   Mascota(){ 
+    this->vida=100;
+    }
+    void EstablecerNombre(std::string nombre) {
+        
+        this->nombre = nombre;
+
+    }
+    ~Mascota() {}
+    void DecirNombre()
+    {
+        std::cout<< "Mi nombre es: "<< this->nombre << std::endl;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,20 @@
+#include <iostream>
+#include <Mascota.hpp>
 
+int main (int argc, char const *argv[])
+{
 
+    std::cout<<"juego de mascota"<<std::endl;
+
+    Mascota m1,m2,m3;
+    m1.EstablecerNombre("firulais");
+    m2.EstablecerNombre("scubi");
+    m3.EstablecerNombre("scubi 2");
+
+    m1.DecirNombre();
+    m1.DecirNombre();
+    m1.DecirNombre();
+
+    return 0; 
+
+}
